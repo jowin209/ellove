2023d2a63963f68059636fd77a556d92ff92640e
jowin209 <jowinray.209@gmail.com>
2026-07-11 10:08:01 +0800

    Hide the why-cinnamoroll page temporarily

--- script/letter.js
+++ script/letter.js
@@ -1,8 +1,16 @@
 const letterContent = document.querySelector(".letter-content");
+const cinnamorollCards = document.querySelector(".why-cinnamoroll");
 
-const unlockDate = new Date("2026-07-12T00:00:00");
+const unlockDate = new Date("2026-07-12T00:00:00"); // TODO: Make sure to set this to the correct date.
 const now = new Date();
 
-if (letterContent && now < unlockDate) {
-    letterContent.innerHTML = "<p class='unavailable'>Come back here by July 12, 2026.</p>";
+if (now < unlockDate) {
+
+    if (letterContent) {
+        letterContent.innerHTML = "<p class='unavailable'>Come back here by July 12, 2026.</p>";
+    }
+
+    if (cinnamorollCards) {
+        cinnamorollCards.innerHTML = "<p class='unavailable'>Learn more by July 12, 2026.</p>";
+    }
 }
